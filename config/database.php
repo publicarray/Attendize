9832b4ae0c5a7d68be9ad2d70d87c7a21a629433
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-09-06 16:25:56 +1000

    Cleanup and http -> https

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST'),
-            'port'	=> env('DB_PORT'),
+            'port'      => env('DB_PORT'),
             'database'  => env('DB_DATABASE'),
             'username'  => env('DB_USERNAME'),
             'password'  => env('DB_PASSWORD'),
