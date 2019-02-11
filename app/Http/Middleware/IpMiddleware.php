d1f752de595b155a72585f56fcb90d764c1143cf
Sebastian Schmidt <publicarray@users.noreply.github.com>
2019-02-11 22:19:56 +1100

    update allowed IP

--- app/Http/Middleware/IpMiddleware.php
+++ app/Http/Middleware/IpMiddleware.php
@@ -8,7 +8,7 @@ class IpMiddleware
 {
     public function handle($request, Closure $next)
     {
-        if (!in_array($request->getClientIp(), ['1.144.109.5', '110.239.26.178', '127.0.0.1']))
+        if (!in_array($request->getClientIp(), ['1.129.110.165', '110.239.26.178', '127.0.0.1']))
         {
             abort(404);
         }
