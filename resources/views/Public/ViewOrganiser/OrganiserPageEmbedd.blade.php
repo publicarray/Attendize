88e9cde5347d738e3ed0730cecdce26f55dfa96e
Sebastian Schmidt <publicarray@users.noreply.github.com>
2019-02-12 10:24:42 +1100

    Open links in a new window from an embedded organiser page

--- resources/views/Public/ViewOrganiser/OrganiserPageEmbedd.blade.php
+++ resources/views/Public/ViewOrganiser/OrganiserPageEmbedd.blade.php
@@ -20,13 +20,13 @@
     <section id="events" class="container-fluid">
     <div class="row">
         <div class="col-md-12">
-            @include('Public.ViewOrganiser.Partials.EventListingPanel',
+            @include('Public.ViewOrganiser.Partials.EventListingPanelEmbedd',
                 [
                     'panel_title' => trans("Public_ViewOrganiser.upcoming_events"),
                     'events'      => $upcoming_events
                 ]
             )
-            @include('Public.ViewOrganiser.Partials.EventListingPanel',
+            @include('Public.ViewOrganiser.Partials.EventListingPanelEmbedd',
                 [
                     'panel_title' => trans("Public_ViewOrganiser.past_events"),
                     'events'      => $past_events
